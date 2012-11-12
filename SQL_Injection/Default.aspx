1b99081008c1cc04d8682ea8cf33b8a4e637b08c
Tim Kara <engintimkara@gmail.com>
2012-11-12 21:50:22 +0100

    ADD Project

---
+++ SQL_Injection/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="SQL_Injection._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Welcome to ASP.NET!
+    </h2>
+    <p>
+        Username:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBoxUser" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Password:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBoxPWD" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="ButtonSend" runat="server" onclick="ButtonSend_Click" 
+            Text="UnSafe" />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Safe" runat="server" onclick="Safe_Click" Text="Server IP" />
+    </p>
+    <p>
+        Name:&nbsp;
+        <asp:Label ID="LabelName" runat="server" Text="null"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </p>
+    <p>
+        &nbsp;Password:&nbsp;
+        <asp:Label ID="LabelPWD" runat="server" Text="null"></asp:Label>
+    </p>
+    <p>
+        SQL:&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="LabelSQL" runat="server" Text="null"></asp:Label>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        To learn more about ASP.NET visit <a href="http://www.asp.net" title="ASP.NET Website">www.asp.nett</a>.
+    </p>
+    <p>
+        You can also find <a href="http://go.microsoft.com/fwlink/?LinkID=152368&amp;clcid=0x409"
+            title="MSDN ASP.NET Docs">documentation on ASP.NET at MSDN</a>.
+    </p>
+</asp:Content>
---
+++ SQL_Injection/Default.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Collections;
+using System.Data.SqlClient;
+using System.Net;
+using System.Text;
+
+namespace SQL_Injection
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        string username, pass, db_n, db_p,sql;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            username = TextBoxUser.Text;
+            pass = TextBoxPWD.Text;
+        }
+
+        protected void ButtonSend_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0; Data source = " + Server.MapPath("~/App_Data/DatabaseTEST.mdb"));
+            conn.Open();
+
+            OleDbCommand cmd = conn.CreateCommand();
+            cmd.CommandText = @"select * from Users where UserName = '" + username + "' and PassWord = '" + pass + "'";
+            sql = cmd.CommandText;
+            try
+            {
+               
+            OleDbDataReader rdr = cmd.ExecuteReader();
+            if (rdr.Read())
+            {
+                db_n = rdr["UserName"].ToString();
+                db_p = rdr["PassWord"].ToString();
+                rdr.Close();
+            }
+            else
+            {
+                db_n = "Unknown and was not found in the system database";
+                db_p = "Unknown and was not found in the system database";
+            }
+            rdr.Close();
+            }
+            catch
+            {
+                db_n = "Deny access to the Secret Info";
+                db_p = "Deny access to the Secret Info";
+            }
+
+            LabelName.Text = db_n;
+            LabelPWD.Text = db_p;
+            LabelSQL.Text = sql;
+
+        }
+
+        protected void Safe_Click(object sender, EventArgs e)
+        {
+
+            String whatIsMyIp = "http://automation.whatismyip.com/n09230945.asp";
+            WebClient wc = new WebClient();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            string requestHtml = "";
+            try
+            {
+                requestHtml = utf8.GetString(wc.DownloadData(whatIsMyIp));
+            }
+            catch (WebException we)
+            {
+                // do something with exception
+                Console.Write(we.ToString());
+            }
+            LabelSQL.Text = "Server external IP : " + requestHtml.ToString();
+        }
+    }
+}
